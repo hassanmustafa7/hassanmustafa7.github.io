5333ae62e8c71a0f9ec00fa32b3ca916917be831
hassanmustafa7 <Sm1793123@gmail.com>
2021-12-02 15:11:02 +0000

    added cannons

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -25,9 +25,11 @@
         let createCannon = cannon.create;
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-        
+        createCannon("top", 250);
+        createCannon("left", 250, 1000);
+        createCannon("right", 600, 1000);
         // example: 
-        createCannon("top", 450);
+        createCannon("top", 650);
         
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
